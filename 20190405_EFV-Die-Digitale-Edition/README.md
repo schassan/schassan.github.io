db169b77b216c2de73154c3fd6e91d38a9fe0722
Torsten Schassan <schassan@hab.de>
2019-04-06 00:18:37 +0200

    Initial commit for presentation

---
+++ 20190405_EFV-Die-Digitale-Edition/README.md
@@ -0,0 +1,13 @@
+### Internationale Tagung "Erschließen, Forschen, Vermitteln: Kulturelles Handeln von Frauen zwischen 1800 und 2000" &#x2014; 5.4.2019
+
+# Erschließen, Forschen, Vermitteln
+
+## Die Digitale Edition
+
+### [Torsten Schaßan](https://orcid.org/0000-0002-8902-4775)
+
+Folien zum Vortrag auf der Tagung in Wöltingerode. Online zu finden unter:
+
+https://schassan.github.io/20190405_EFV-Die-Digitale-Edition
+
+Alle Inhalte [CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/), [Torsten Schaßan](https://orcid.org/0000-0002-8902-4775)
---
+++ 20190405_EFV-Die-Digitale-Edition/slide_config.js
@@ -0,0 +1,42 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Erschließen, Forschen, Vermitteln', //<br>
+    subtitle: 'Die Digitale Edition',
+    eventInfo: {
+      title: 'Internat. Tagung &quot;Erschließen, Forschen, Vermitteln: Kulturelles Handeln von Frauen zwischen 1800 und 2000&quot;',
+      date: '5.4.2019'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/HAB_logo_tiny.png',
+    fonts: [
+      'Spectral:regular,semibold,italic,semibolditalic',
+      'Roboto:regular,medium,italic,mediumitalic',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Torsten Schaßan',
+    company: 'Digitale Editionen<br>Herzog August Bibliothek Wolfenbüttel',
+    mail: 'schassan@hab.de',
+    orcid: '0000-0002-8902-4775',
+    twitter: '@schassan',
+    www: 'http://www.hab.de',
+    github: 'http://github.com/schassan'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
