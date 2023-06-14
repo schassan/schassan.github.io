d4d2437dbcd6e28a46bdcaf1d6bd06988595f80f
Torsten Schaßan <schassan@googlemail.com>
2023-06-14 10:45:57 +0200

    Folien zum Vortrag "Handschriftenportal ohne Beta" am 13.6.2023

---
+++ 20230613_HSP-ohne-Beta/README.md
@@ -0,0 +1,12 @@
+### Handschriftenportal ohne Beta &#x2014; 13.06.2023
+
+# Daten und ihre Wege ins System
+
+
+### [Torsten Schaßan](https://orcid.org/0000-0002-8902-4775)
+
+Folien zur Präsentation. Online zu finden unter:
+
+https://schassan.github.io/20230613_HSP-ohne-Beta
+
+Alle Inhalte [CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/), [Torsten Schaßan](https://orcid.org/0000-0002-8902-4775)
---
+++ 20230613_HSP-ohne-Beta/slide_config.js
@@ -0,0 +1,35 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Daten und ihre Wege ins System', //<br>
+    subtitle: '',
+    eventInfo: {
+      title: 'Handschriftenportal ohne Beta',
+      date: '13.06.2023'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/HAB_logo_tiny.png',
+    fonts: [
+      'Spectral:regular,semibold,italic,semibolditalic',
+      'Roboto:regular,medium,italic,mediumitalic',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [
+  {
+    name: 'Torsten Schaßan',
+    mail: 'schassan@hab.de',
+    orcid: '0000-0002-8902-4775',
+    twitter: '@schassan',
+    www: 'http://www.hab.de',
+    github: 'http://github.com/schassan'
+  } ]
+};
+
